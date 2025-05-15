4021c4749caf48aa64e278dfe6eebe843edf70a4
Weslley H V Ferraz <weslleyhenrique800@gmail.com>
2025-05-15 19:58:21 -0400

    a

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Olá Mundo, Weslley!</Text>
+      <Button
+        title="Clique aqui"
+        onPress={() => {
+          alert('Botão clicado!');
+        }}></Button>
       <StatusBar style="auto" />
     </View>
   );
