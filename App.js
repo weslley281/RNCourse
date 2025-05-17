b7c9afd5c4ecf7fb64a6f7a2cd14b05f6847435f
Weslley H V Ferraz <weslleyhenrique800@gmail.com>
2025-05-16 20:34:35 -0400

    a

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá Mundo, Weslley!</Text>
-      <Button
-        title="Clique aqui"
-        onPress={() => {
-          alert('Botão clicado!');
-        }}></Button>
-      <StatusBar style="auto" />
+      <Text
+        style={styles.text1}
+      >Olá Mundo, Weslley!</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={
+          () => Alert.alert('Saudações', 'Olá Mundo, Weslley!')}
+      >
+        <Text style={styles.text2}>Clique aqui</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -21,5 +24,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text1: {
+    fontSize: 30,
+    color: 'darkgray',
+    fontWeight: 'bolder',
+    marginBottom: 20,
+  },
+  text2: {
+    fontSize: 20,
+    color: 'whitesmoke',
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: 'darkblue',
+    padding: 10,
+    borderRadius: 5,
   },
 });
